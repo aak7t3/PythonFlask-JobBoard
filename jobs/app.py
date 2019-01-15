e2c8a76e16e54e71ed16c3ee3905a981c41f3baf
aak7t3 <aak7t3@mail.umsl.edu>
2019-01-15 00:34:53 -0600

    Update app.py

--- jobs/app.py
+++ jobs/app.py
@@ -14,8 +14,9 @@ def open_connection():
 
 def execute_sql(sql, values = (), commit=False, single=False):
     connection = open_connection()
+    cursor = connection.execute(sql, values)
     if commit == True:
-        results = connection.commit
+        results = connection.commit()
     else:
         results = cursor.fetchone() if single else cursor.fetchall()
 
